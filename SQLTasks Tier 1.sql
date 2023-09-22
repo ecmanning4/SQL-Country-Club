667e1230545144b3ba022fdbe17709e5d915c826
ecmanning4 <136523373+ecmanning4@users.noreply.github.com>
2023-09-22 17:15:30 -0400

    SQLTasks Tier 1
    1-9 Answered

--- SQLTasks Tier 1.sql
+++ SQLTasks Tier 1.sql
@@ -30,50 +30,111 @@ Before starting with the questions, feel free to take your time,
 exploring the data, and getting acquainted with the 3 tables. */
 
 
-/* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
-
-
-/* Q2: How many facilities do not charge a fee to members? */
-
-
-/* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
+SELECT
+    facid,
+    NAME
+FROM
+    Facilities
+WHERE
+    membercost = 0;
+    /* Q2: How many facilities do not charge a fee to members? */
+    4
+    /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-
-
-/* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
+SELECT
+    facid,
+    NAME,
+    membercost,
+    monthlymaintenance
+FROM
+    Facilities
+WHERE
+    membercost > 0 AND membercost <(monthlymaintenance * 0.20);
+    /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
-
-
-/* Q5: Produce a list of facilities, with each labelled as
+SELECT
+    *
+FROM
+    Facilities
+WHERE
+    facid IN(1, 5);
+    /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
-
-
-/* Q6: You'd like to get the first and last name of the last member(s)
+SELECT NAME
+    ,
+    monthlymaintenance,
+    CASE WHEN monthlymaintenance > 100 THEN 'expensive' ELSE 'cheap'
+END AS 'expensive_vs_cheap'
+FROM
+    Facilities;
+    /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
-
-
+SELECT
+    firstname,
+    surname
+FROM
+    Members
+WHERE
+    joindate IN(
+    SELECT
+        MAX(joindate)
+    FROM
+        Members
+);
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
-
-
-/* Q8: Produce a list of bookings on the day of 2012-09-14 which
+SELECT DISTINCT
+    CONCAT(NAME, ' ', surname, ' ', firstname) AS tennis_bookings
+FROM
+    Bookings
+LEFT JOIN Facilities USING(facid)
+LEFT JOIN Members USING(memid)
+WHERE
+    facid IN(0, 1) AND memid != 0
+ORDER BY
+    surname,
+    firstname;
+    /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
 different costs to members (the listed costs are per half-hour 'slot'), and
 the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
-
-
-/* Q9: This time, produce the same result as in Q8, but using a subquery. */
-
+SELECT
+    CONCAT(NAME, ' ', surname, ' ', firstname) AS september_14_bookings,
+    CASE WHEN memid = 0 THEN slots * guestcost ELSE slots * membercost
+END AS cost
+FROM
+    Bookings
+LEFT JOIN Facilities USING(facid)
+LEFT JOIN Members USING(memid)
+WHERE
+    starttime LIKE '2012-09-14%'
+HAVING cost > 30
+ORDER BY
+    cost
+DESC
+    ;
+    /* Q9: This time, produce the same result as in Q8, but using a subquery. */
+SELECT
+    CONCAT(NAME, ' ', surname, ' ', firstname) AS september_14_bookings,
+    CASE WHEN memid = 0 THEN slots * guestcost ELSE slots * membercost
+END AS cost
+FROM (SELECT * 
+      FROM Bookings
+      WHERE starttime LIKE '2012-09-14%') AS september_14_bookings
+LEFT JOIN Facilities USING(facid)
+LEFT JOIN Members USING(memid)
+WHERE (memid = 0 and slots * guestcost >30) or (memid != 0 and membercost > 30)
+ORDER BY cost DESC;
 
 /* PART 2: SQLite
 /* We now want you to jump over to a local instance of the database on your machine. 
